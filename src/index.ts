aabf7c178cf87f98091df52aa4befcd7a0bbcb0e
Damian Osipiuk <damian.osipiuk@codilime.com>
2020-03-06 18:19:06 +0100

    Add feature to update existing test run if refs match. Remove test process output. Use testrail-js-api lib.

--- src/index.ts
+++ src/index.ts
@@ -1,165 +1,159 @@
+import fs from "fs";
 import path from "path";
-import { loadJSON, Status, separator } from "./utils";
-import { TestRail } from "./testrail";
-import {
-  Reporter,
-  Options,
-  Meta,
-  TestRunInfo,
-  TaskResult,
-  TestResult,
-} from "./interfaces";
+import moment from "moment";
+import { TestRail, TestStatus, Run, AddResultForCase } from "testrail-js-api";
 
-export class TestrailReporter extends Reporter {
+interface Config<T = number> {
+  enabled: boolean;
+  host: string;
+  user: string;
+  apiKey: string;
+  projectId: T;
+  planId?: T;
+  suiteId: T;
+  runName: string;
+  runDescription?: string;
+  reference?: string;
+  branchEnv: string;
+  buildNoEnv: string;
+  dateFormat: string;
+  caseMeta: string;
+  runCloseAfterDays?: number;
+  uploadScreenshots: boolean;
+}
+
+export interface TestResult {
+  name: string;
+  meta: Meta;
+  caseId: number;
+  testRunInfo: TestRunInfo;
+  testStatus: {
+    value: TestStatus;
+    text: string;
+    color: string;
+  };
+}
+
+export interface Meta {
+  [key: string]: string;
+}
+
+export interface TaskResult {
+  passedCount: number;
+  failedCount: number;
+  skippedCount: number;
+}
+
+export interface Screenshot {
+  screenshotPath: string;
+  thumbnailPath: string;
+  userAgent: string;
+  quarantineAttempt: number;
+  takenOnFail: boolean;
+}
+
+export interface TestRunInfo {
+  errs: any[];
+  warnings: string[];
+  durationMs: number;
+  unstable: boolean;
+  screenshotPath: string;
+  screenshots: Screenshot[];
+  quarantine: { [key: string]: { passed: boolean } };
+  skipped: boolean;
+}
+
+const Status = {
+  Passed: {
+    value: TestStatus.Passed,
+    text: "PASSED",
+    color: "yellow",
+  },
+  Blocked: {
+    value: TestStatus.Blocked,
+    text: "SKIPPED",
+    color: "green",
+  },
+  Failed: {
+    value: TestStatus.Failed,
+    text: "FAILED",
+    color: "red",
+  },
+};
+
+const loadJSON = (file: string) => {
+  const data = fs.readFileSync(file, { encoding: "utf8" });
+
+  return JSON.parse(data);
+};
+
+const prepareConfig = (options: Config = {} as any): Config => {
+  const config: Config<string> = Object.assign(loadJSON(path.join(process.cwd(), ".testrailrc")), options);
+
+  return {
+    enabled: process.env.TESTRAIL_ENABLED === "true" || config.enabled || false,
+    host: process.env.TESTRAIL_HOST || config.host,
+    user: process.env.TESTRAIL_USER || config.user,
+    apiKey: process.env.TESTRAIL_API_KEY || config.apiKey,
+    projectId: Number((process.env.TESTRAIL_PROJECT_ID || config.projectId || "").replace("P", "").trim()),
+    planId: Number((process.env.TESTRAIL_PLAN_ID || config.planId || "").replace("R", "").trim()),
+    suiteId: Number((process.env.TESTRAIL_SUITE_ID || config.suiteId || "").replace("S", "").trim()),
+    runName: process.env.TESTRAIL_RUN_NAME || config.runName || "%BRANCH%#%BUILD% - %DATE%",
+    runDescription: process.env.TESTRAIL_RUN_DESCRIPTION || config.runDescription,
+    reference: process.env.TESTRAIL_REFERENCE || config.reference,
+    branchEnv: process.env.TESTRAIL_BRANCH_ENV || config.branchEnv || "BRANCH",
+    buildNoEnv: process.env.TESTRAIL_BUILD_NO_ENV || config.buildNoEnv || "BUILD_NUMBER",
+    dateFormat: process.env.TESTRAIL_DATE_FORMAT || config.dateFormat || "YYYY-MM-DD HH:mm:ss",
+    caseMeta: process.env.TESTRAIL_CASE_META || config.caseMeta || "CID",
+    runCloseAfterDays: Number(process.env.TESTRAIL_RUN_CLOSE_AFTER_DAYS) || config.runCloseAfterDays,
+    uploadScreenshots: process.env.TESTRAIL_UPLOAD_SCREENSHOT == "true" || config.uploadScreenshots || false,
+  };
+};
+
+const prepareReportName = (config: Config, branch: string, buildNo: string, userAgents: string[]) => {
+  const date = moment().format(config.dateFormat);
+  return config.runName
+    .replace("%BRANCH%", branch)
+    .replace("%BUILD%", buildNo)
+    .replace("%DATE%", date)
+    .replace("%AGENTS%", `(${userAgents.join(", ")})`);
+};
+
+const prepareReference = (config: Config, branch: string, buildNo: string) => {
+  return config.reference ? config.reference.replace("%BRANCH%", branch).replace("%BUILD%", buildNo) : "";
+};
+
+export class TestcafeTestrailReporter {
   noColors: boolean;
+  formatError: any;
 
-  options: Options;
-  testrailAPI!: TestRail;
-  startTime?: number;
-  userAgents?: string[];
-  testCount?: number;
-  testResults: TestResult[];
+  private config: Config;
+  private branch: string;
+  private buildNo: string;
 
-  constructor(config: Options) {
-    super();
+  userAgents!: string[];
+  results: AddResultForCase[];
+  screenshots: {
+    [key: string]: Screenshot[];
+  };
+
+  constructor() {
+    this.config = prepareConfig();
+    this.branch = process.env[this.config.branchEnv] || "master";
+    this.buildNo = process.env[this.config.buildNoEnv] || "unknown";
 
     this.noColors = false;
-    this.testResults = [];
-
-    this.options = {
-      testrailEnabled:
-        process.env.TESTRAIL_ENABLE == "true" ||
-        config.testrailEnabled ||
-        false,
-      host: process.env.TESTRAIL_HOST || config.host,
-      user: process.env.TESTRAIL_USER || config.user,
-      apiKey: process.env.TESTRAIL_API_KEY || config.apiKey,
-      project: process.env.TESTRAIL_PROJECT || config.project,
-      projectId: process.env.TESTRAIL_PROJECT_ID || config.projectId,
-      plan: process.env.TESTRAIL_PLAN || config.plan,
-      planId: process.env.TESTRAIL_PLAN_ID || config.planId,
-      suite: process.env.TESTRAIL_SUITE || config.suite || "Master",
-      suiteId: process.env.TESTRAIL_SUITE_ID || config.suiteId,
-      caseMeta: process.env.TESTRAIL_CASE_META || config.caseMeta || "CID",
-      runName: process.env.TESTRAIL_RUN_NAME || config.runName,
-      runDescription:
-        process.env.TESTRAIL_RUN_DESCRIPTION || config.runDescription,
-      runCloseAfterDays:
-        process.env.TESTRAIL_RUN_CLOSE_AFTER_DAYS || config.runCloseAfterDays,
-      uploadScreenshots:
-        process.env.TESTRAIL_UPLOAD_SCREENSHOT == "true" ||
-        config.uploadScreenshots ||
-        false,
-    };
+    this.results = [];
+    this.screenshots = {};
   }
 
-  renderErrors(errs: object[]) {
-    this.setIndent(2).newline();
-
-    errs.forEach((err, idx) => {
-      var prefix = this.chalk.red(idx + 1 + ") ");
-
-      this.newline()
-        .write(this.formatError(err, prefix))
-        .newline()
-        .newline();
-    });
-    this.setIndent(0);
-  }
-
-  async reportTaskStart(
-    startTime: number,
-    userAgents: string[],
-    testCount: number
-  ) {
-    this.startTime = startTime;
+  reportTaskStart = async (startTime: number, userAgents: string[]) => {
     this.userAgents = userAgents;
-    this.testCount = testCount;
+  };
 
-    this.newline()
-      .write(separator)
-      .newline()
-      .write("Running tests in:")
-      .newline()
-      .setIndent(2);
+  reportFixtureStart = async () => {};
 
-    userAgents.forEach((userAgent) => {
-      this.write(this.chalk.blue(userAgent)).newline();
-    });
-
-    this.setIndent(0);
-
-    // Check if env was set up
-    if (this.options.testrailEnabled) {
-      if (
-        !this.options.host ||
-        !this.options.user ||
-        !this.options.apiKey ||
-        (!this.options.project && !this.options.projectId)
-      ) {
-        if (!this.options.host) {
-          this.newline()
-            .write(
-              this.chalk.red.bold(
-                "Error: You have to specify Testrail hostname via env or config file."
-              )
-            )
-            .newline();
-        }
-        if (!this.options.user) {
-          this.newline()
-            .write(
-              this.chalk.red.bold(
-                "Error: You have to specify Testrail username via env or config file."
-              )
-            )
-            .newline();
-        }
-        if (!this.options.apiKey) {
-          this.newline()
-            .write(
-              this.chalk.red.bold(
-                "Error: You have to specify Testrail api key via env or config file."
-              )
-            )
-            .newline();
-        }
-        if (!this.options.project && !this.options.projectId) {
-          this.newline()
-            .write(
-              this.chalk.red.bold(
-                "Error: You have to specify Testrail project or projectId via env or config file."
-              )
-            )
-            .newline();
-        }
-        process.exit(1);
-      }
-
-      this.testrailAPI = new TestRail(
-        {
-          host: this.options.host,
-          user: this.options.user,
-          password: this.options.apiKey,
-        },
-        this
-      );
-
-      return await this.testrailAPI.testConnection();
-    }
-  }
-
-  reportFixtureStart(name: string, path: string, meta: Meta) {
-    this.setIndent(0)
-      .newline()
-      .write(`[Fixture] ${this.chalk.blue(name)}`)
-      .newline();
-  }
-
-  reportTestDone(name: string, testRunInfo: TestRunInfo, meta: Meta) {
-    const durationFormatted = (this.moment.duration(
-      testRunInfo.durationMs
-    ) as any).format("h[h] mm[m] ss[s]");
+  async reportTestDone(name: string, testRunInfo: TestRunInfo, meta: Meta) {
     const hasErr = testRunInfo.errs.length;
 
     let testStatus = null;
@@ -173,121 +167,143 @@ export class TestrailReporter extends Reporter {
     }
 
     let caseId = 0;
-    if (meta[this.options.caseMeta]) {
-      caseId = Number(meta[this.options.caseMeta].replace("C", "").trim());
+    if (meta[this.config.caseMeta]) {
+      caseId = Number(meta[this.config.caseMeta].replace("C", "").trim());
     }
 
-    this.testResults.push({
-      name,
-      meta,
-      caseId,
-      testRunInfo: testRunInfo,
-      durationFormatted: durationFormatted,
-      testStatus: testStatus,
-    });
+    if (caseId > 0) {
+      const errorLog = testRunInfo.errs
+        .map((x: object) => {
+          const formatted = this.formatError(x).replace(
+            /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+            ""
+          );
 
-    this.setIndent(2)
-      .write(`[Test] ${this.chalk.blue(name)}`)
-      .write(">")
-      .write(this.chalk.keyword(testStatus.color)(testStatus.text))
-      .write(`(${durationFormatted})`)
-      .newline();
+          return formatted;
+        })
+        .join("\n");
 
-    if (hasErr > 0) {
-      this.renderErrors(testRunInfo.errs);
-    }
-  }
-
-  async reportTaskDone(
-    endTime: number,
-    passed: number,
-    warnings: string[],
-    result: TaskResult
-  ) {
-    const durationFormatted = (this.moment.duration(
-      endTime - (this.startTime || 0)
-    ) as any).format("h[h] mm[m] ss[s]");
-
-    const failedTests = this.testResults.filter(
-      (result) => result.testStatus.value === Status.Failed.value
-    );
-
-    if (failedTests.length > 0) {
-      // Display failed tests summary
-      this.setIndent(0)
-        .newline()
-        .write(separator)
-        .newline()
-        .write(`${this.chalk.red.bold("Failed tests summary")}`)
-        .newline()
-        .newline();
-      failedTests.forEach((testCase) => {
-        this.setIndent(2)
-          .write(`[Test] ${this.chalk.blue(testCase.name)}`)
-          .write(">")
-          .write(
-            this.chalk.keyword(testCase.testStatus.color)(
-              testCase.testStatus.text
-            )
-          )
-          .write(`(${testCase.durationFormatted})`);
-        this.renderErrors(testCase.testRunInfo.errs);
+      this.results.push({
+        case_id: caseId,
+        status_id: testStatus.value,
+        comment: `Test ${testStatus.text}\n${errorLog}`,
       });
-    }
-
-    this.setIndent(0)
-      .newline()
-      .write(separator)
-      .newline()
-      .write("Tests:")
-      .setIndent(1);
-
-    if (result.passedCount) {
-      this.write(
-        this.chalk.keyword(Status.Passed.color)(
-          `${result.passedCount} ${Status.Passed.text}`
-        )
-      );
-    }
-    if (result.skippedCount) {
-      if (result.passedCount) {
-        this.write("/");
+      if (testRunInfo.screenshots.length) {
+        this.screenshots[caseId] = testRunInfo.screenshots;
       }
-      this.write(
-        this.chalk.keyword(Status.Blocked.color)(
-          `${result.skippedCount} ${Status.Blocked.text}`
-        )
-      );
-    }
-    if (result.failedCount) {
-      if (result.passedCount || result.skippedCount) {
-        this.write("/");
-      }
-      this.write(
-        this.chalk.keyword(Status.Failed.color)(
-          `${result.failedCount} ${Status.Failed.text}`
-        )
-      );
-    }
-    this.write(`(${this.testCount} total)`).setIndent(0);
-
-    this.newline()
-      .write(`Time: ${durationFormatted}`)
-      .newline()
-      .newline();
-
-    if (this.options.testrailEnabled) {
-      await this.testrailAPI.publishTestRun(
-        this.options,
-        this.testResults,
-        this.userAgents || [""]
-      );
+    } else {
+      console.warn(`[TestRail] Test missing the TestRail Case ID in test metadata: ${name}`);
     }
   }
+
+  reportTaskDone = async (endTime: number, passed: number, warnings: string[], result: TaskResult) => {
+    const { enabled, host, user, apiKey, projectId } = this.config;
+    try {
+      if (enabled && host && user && apiKey) {
+        if (this.results.length) {
+          const testrailAPI = new TestRail(host, user, apiKey);
+
+          const { value: runs } = await testrailAPI.getRuns(projectId, { is_completed: 0 });
+
+          const runName = prepareReportName(this.config, this.branch, this.buildNo, this.userAgents);
+          const refs = prepareReference(this.config, this.branch, this.buildNo);
+          const caseIdList = this.results.map((result) => result.case_id);
+
+          const existingRun = runs.find((run) => run.refs === refs);
+
+          let run: Run;
+          if (existingRun) {
+            run = existingRun;
+            const { value: tests } = await testrailAPI.getTests(existingRun.id);
+            const currentCaseIds = tests.map((test) => test.case_id);
+            const additionalDescription = "\n" + this.config.runDescription
+
+            await testrailAPI.updateRun(existingRun.id, {
+              description: existingRun.description.replace(additionalDescription, '') + additionalDescription,
+              case_ids: [...currentCaseIds, ...caseIdList],
+            });
+
+            console.info(`[TestRail] Test run updated successfully: ${runName}`);
+          } else {
+            const payload = {
+              suite_id: this.config.suiteId,
+              include_all: false,
+              case_ids: caseIdList,
+              name: runName,
+              description: this.config.runDescription,
+              refs,
+            };
+
+            if (this.config.planId) {
+              const { value: planEntry } = await testrailAPI.addPlanEntry(this.config.planId, payload);
+              run = planEntry.runs[0];
+            } else {
+              await this.closeOldRuns(testrailAPI, this.config);
+              const { value: newRun } = await testrailAPI.addRun(this.config.projectId, payload);
+              run = newRun;
+            }
+
+            console.info(`[TestRail] Test run added successfully: ${runName}`);
+          }
+
+          await this.publishTestResults(testrailAPI, run, this.results);
+        } else {
+          console.warn("[TestRail] No test case data found to publish");
+        }
+      }
+    } catch (error) {
+      console.error("[TestRail] Sending report to TestRail failed", error.toString());
+    }
+  };
+
+  closeOldRuns = async (testrailAPI: TestRail, config: Config) => {
+    if (config.runCloseAfterDays) {
+      const { value: runs } = await testrailAPI.getRuns(config.projectId, { is_completed: 0 });
+      if (runs?.length) {
+        runs.forEach(async (run) => {
+          const shouldClose = moment.unix(run.created_on) <= moment().subtract(config.runCloseAfterDays, "days");
+          if (shouldClose) {
+            console.info(`[TestRail] Closing test run ${run.id}: ${run.name}`);
+            await testrailAPI.closeRun(run.id);
+          }
+        });
+      } else {
+        console.error("[TestRail] Error during test runs closing");
+      }
+    }
+  };
+
+  publishTestResults = async (testrailAPI: TestRail, run: Run, resultsToPush: AddResultForCase[]) => {
+    const runId = run.id;
+    const { value: results } = await testrailAPI.addResultsForCases(runId, resultsToPush);
+    const { value: tests } = await testrailAPI.getTests(runId);
+
+    if (this.config.uploadScreenshots) {
+      for (const testResult of resultsToPush) {
+        const test = tests.find((test) => test.case_id === testResult.case_id);
+        const result = results.find((result) => result.test_id === test?.id);
+        if (result) {
+          for (const screenshot of this.screenshots[testResult.case_id]) {
+            console.log("screenshot", screenshot);
+            const { response, value } = await testrailAPI.addAttachmentToResult(result.id, screenshot.screenshotPath);
+            console.log("response", response, value);
+          }
+        } else {
+          console.error(
+            `[TestRail] Could not upload screenshot for a failed test. Case ID: ${testResult.caseId}. Test ID: ${test?.id}`
+          );
+        }
+      }
+    }
+
+    if (results.length == 0) {
+      console.info("[TestRail] No Data has been published to TestRail");
+    } else {
+      console.info("[TestRail] Test results added to the TestRail successfully");
+    }
+  };
 }
 
 module.exports = function() {
-  const config = loadJSON(path.join(process.cwd(), ".testrailrc"));
-
-  return new TestrailReporter(config);
+  return new TestcafeTestrailReporter();
 };

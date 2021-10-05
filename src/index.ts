2527a01b8c1a52c4957abeae7533c5f64c1012eb
Jérémy Torralba <jeremy.torralba@agathalife.com>
2021-10-05 14:38:37 +0200

    feat: add test duration in TestRail

--- src/index.ts
+++ src/index.ts
@@ -199,11 +199,13 @@ class TestcafeTestrailReporter {
           return formatted;
         })
         .join("\n");
-
+      
+      const testDuration = (testRunInfo.durationMs / 1000).toString() + "s";
       this.results.push({
         case_id: caseId,
         status_id: testStatus.value,
         comment: `Test ${testStatus.text}\n${errorLog}`,
+        elapsed: testDuration
       });
       if (testRunInfo.screenshots.length) {
         this.screenshots[caseId] = testRunInfo.screenshots;
